7d8869e497fe1b8b2f443b84f17e5e8a817b1af1
luckschavalo <lucaschl1007@gmail.com>
2025-04-16 18:50:23 -0600

    inicio del caso practico
    creacion del proyecto completando los puntos del 1 al 3

---
+++ CasoPractico/src/test/java/CasoPractico02/Caso02/CasoPractico02ApplicationTests.java
@@ -0,0 +1,13 @@
+package CasoPractico02.Caso02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoPractico02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
